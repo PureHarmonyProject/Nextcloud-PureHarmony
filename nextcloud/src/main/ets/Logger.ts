a6f2c7462605690db317fcfbdfd2ada36c0bf664
Moody Liu <mooodyhunter@outlook.com>
2025-10-18 00:41:47 +0800

    login UX updates

--- nextcloud/src/main/ets/Logger.ts
+++ nextcloud/src/main/ets/Logger.ts
@@ -1,20 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 import hilog from '@ohos.hilog'
 
+const DOMAIN = 0xABAB;
+
 export class Logger {
-    static Debug(component: string, message: string) {
-        hilog.debug(0x0001, component, message)
-    }
+  static Debug(component: string, message: string) {
+    hilog.debug(DOMAIN, component, message)
+  }
 
-    static Info(component: string, message: string) {
-        hilog.info(0x0001, component, message)
-    }
+  static Info(component: string, message: string) {
+    hilog.info(DOMAIN, component, message)
+  }
 
-    static Warn(component: string, message: string) {
-        hilog.warn(0x0001, component, message)
-    }
+  static Warn(component: string, message: string) {
+    hilog.warn(DOMAIN, component, message)
+  }
 
-    static Error(component: string, message: string) {
-        hilog.error(0x0001, component, message)
-    }
+  static Error(component: string, message: string) {
+    hilog.error(DOMAIN, component, message)
+  }
 }
